c04d8f52c4c1505df4b89c0cdccaa103052661aa
098suraj <30719688+098suraj@users.noreply.github.com>
2022-01-23 02:54:21 +0530

    chidiya

---
+++ app/src/main/java/com/heyletscode/ihavetofly/GameActivity.java
@@ -0,0 +1,53 @@
+package com.heyletscode.ihavetofly;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class GameActivity extends AppCompatActivity {
+
+    private GameView gameView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        final Point point = new Point();
+        getWindowManager().getDefaultDisplay().getSize(point);
+
+        gameView = new GameView(this, point.x, point.y);
+        setContentView(gameView);
+
+    }
+
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        gameView.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        gameView.resume();
+    }
+}
---
+++ app/src/main/java/com/heyletscode/ihavetofly/MainActivity.java
@@ -0,0 +1,84 @@
+package com.heyletscode.ihavetofly;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean isMute;
+    TextView textView;
+    ArrayList<Question> questionArrayList=new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.text_view);
+
+
+
+      //  textView.setText(questionArrayList.get(0).getQuestion());
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+                FirebaseDatabase database = FirebaseDatabase.getInstance();
+                DatabaseReference myRef = database.getReference("message");
+                myRef.setValue("Hello, World!");
+            }
+        });
+
+        TextView highScoreTxt = findViewById(R.id.highScoreTxt);
+
+        final SharedPreferences prefs = getSharedPreferences("game", MODE_PRIVATE);
+        highScoreTxt.setText("HighScore: " + prefs.getInt("highscore", 0));
+
+        isMute = prefs.getBoolean("isMute", false);
+
+        final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
+
+        if (isMute)
+            volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+        else
+            volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                isMute = !isMute;
+                if (isMute)
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_off_black_24dp);
+                else
+                    volumeCtrl.setImageResource(R.drawable.ic_volume_up_black_24dp);
+
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
+
+            }
+        });
+
+    }
+}
